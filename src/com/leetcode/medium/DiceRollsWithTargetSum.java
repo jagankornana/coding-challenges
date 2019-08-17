5e190f921cc89b11364da718141b7a3b19072851
Jagan <jagan.kornana@gmail.com>
2019-08-17 14:54:54 +0530

    Number of Dice Rolls With Target Sum (DP) v_2.0

--- src/com/leetcode/medium/DiceRollsWithTargetSum.java
+++ src/com/leetcode/medium/DiceRollsWithTargetSum.java
@@ -1,10 +1,8 @@
 package com.leetcode.medium;
 
-import java.util.Arrays;
-
 public class DiceRollsWithTargetSum {
 
-    private static int[][] memory;
+    private int prime = (int) (1e9 + 7);
 
     public static void main(String[] args) {
         DiceRollsWithTargetSum targetSum = new DiceRollsWithTargetSum();
@@ -16,33 +14,21 @@ public class DiceRollsWithTargetSum {
     }
 
     private int numRollsToTarget(int d, int f, int target) {
-        memory = new int[d + 1][target + 1];
-        for (int[] arr : memory) {
-            Arrays.fill(arr, Integer.MAX_VALUE);
-        }
-        return rollDice(d, f, target);
+        Integer[][] memory = new Integer[d + 1][target + 1];
+        memory[0][0] = 1;
+        return rollDice(d, f, target, memory);
     }
 
-    private int rollDice(int d, int f, int target) {
-        if (d == 1) {
-            if (target <= f) {
-                return 1;
-            } else {
-                return 0;
-            }
-        }
+    private int rollDice(int d, int f, int target, Integer[][] memory) {
+        if (target < 0) return 0;
+        if (d == 0 && target > 0) return 0;
+        if (memory[d][target] != null) return memory[d][target];
         int count = 0;
         for (int i = 1; i <= f; i++) {
-            if (target - i > 0) {
-                if (memory[d - 1][target - i] != Integer.MAX_VALUE) {
-                    count += memory[d - 1][target - i];
-                    count %= 1000000007;
-                } else {
-                    count += rollDice(d - 1, f, target - i);
-                    memory[d - 1][target - i] = count % 1000000007;
-                }
-            }
+            count += rollDice(d - 1, f, target - i, memory);
+            count %= prime;
         }
+        memory[d][target] = count;
         return count;
     }
 }
