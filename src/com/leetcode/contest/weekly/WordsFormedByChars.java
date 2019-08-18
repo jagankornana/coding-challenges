a1fa2e8eff267bbc2735661f4b97ca25f5167066
Jagan <jagan.kornana@gmail.com>
2019-08-18 11:28:41 +0530

    Find words that can be formed by characters

---
+++ src/com/leetcode/contest/weekly/WordsFormedByChars.java
@@ -0,0 +1,46 @@
+package com.leetcode.contest.weekly;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class WordsFormedByChars {
+
+    public static void main(String[] args) {
+        WordsFormedByChars wordsFormedByChars = new WordsFormedByChars();
+        System.out.println(wordsFormedByChars.countCharacters(new String[]{"cat", "bt", "hat", "tree"}, "atach"));
+        System.out.println(wordsFormedByChars.countCharacters(new String[]{"hello", "world", "leetcode"}, "ollhe"));
+    }
+
+    private int countCharacters(String[] words, String chars) {
+        Map<Character, Integer> map = new HashMap<>();
+        int result = 0;
+        for (char ch : chars.toCharArray()) {
+            Integer count = map.get(ch);
+            if (count != null) {
+                count++;
+                map.put(ch, count);
+            } else {
+                map.put(ch, 1);
+            }
+        }
+
+        for (String word : words) {
+            Map<Character, Integer> temp = new HashMap<>(map);
+            boolean flag = true;
+            for (int i = 0; i < word.length(); i++) {
+                char ch = word.charAt(i);
+                Integer count = temp.get(ch);
+                if (count == null || count < 1) {
+                    flag = false;
+                    break;
+                }
+                temp.put(ch, --count);
+            }
+            if (flag) {
+                result += word.length();
+            }
+        }
+
+        return result;
+    }
+}
