03e37d2d62b5f216bfe9ed3cb94b679ff740c004
Jagan <jagan.kornana@gmail.com>
2019-08-19 23:04:34 +0530

    Last Substring in Lexicographical Order

--- src/com/leetcode/contest/weekly/LastSubstring.java
+++ src/com/leetcode/contest/weekly/LastSubstring.java
@@ -1,8 +1,5 @@
 package com.leetcode.contest.weekly;
 
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * Given a string s, return the last substring of s in lexicographical order.
  */
@@ -12,30 +9,20 @@ public class LastSubstring {
         LastSubstring sol = new LastSubstring();
         System.out.println(sol.lastSubstring("abab"));
         System.out.println(sol.lastSubstring("leetcode"));
-        System.out.println(sol.lastSubstring("aaaaaaaaaa"));
         System.out.println(sol.lastSubstring("xbylisvborylklftlkcioajuxwdhahdgezvyjbgaznzayfwsaumeccpfwamfzmkinezzwobllyxktqeibfoupcpptncggrdqbkji"));
     }
 
     private String lastSubstring(String s) {
-        int len = s.length();
-        List<Integer> indexes = new ArrayList<>();
         char ch = 'a';
+        String maxStr = "";
+        int len = s.length();
         for (int i = 0; i < len; i++) {
-            if (s.charAt(i) > ch) {
-                indexes.clear();
+            if (ch <= s.charAt(i) && maxStr.compareTo(s.substring(i)) <= 0) {
                 ch = s.charAt(i);
-                indexes.add(i);
-            } else if (s.charAt(i) == ch) {
-                indexes.add(i);
+                maxStr = s.substring(i);
             }
+            while (i < len - 1 && s.charAt(i) == s.charAt(i + 1)) i++;
         }
-        String result = s.substring(indexes.get(0));
-        for (int i = 1; i < indexes.size(); i++) {
-            if (result.compareTo(s.substring(indexes.get(i))) < 0) {
-                result = s.substring(indexes.get(i));
-            }
-        }
-
-        return result;
+        return maxStr;
     }
 }
