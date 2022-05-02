6ea98282fcd7744dfb42b01425a54810f9d2800e
Jagan <jagan.kornana@gmail.com>
2022-05-02 11:54:38 +0530

    refactoring

--- src/com/leetcode/problems/Practice.java
+++ src/com/leetcode/problems/Practice.java
@@ -2,16 +2,6 @@ package com.leetcode.problems;
 
 public class Practice {
   public static void main(String[] args) {
-    System.out.println(gcd(500, 246));
-  }
 
-  private static int gcd(int a, int b) {
-    if (a == 0) {
-      return b;
-    } else if (b == 0) {
-      return a;
-    } else {
-      return gcd(b, a % b);
-    }
   }
 }
